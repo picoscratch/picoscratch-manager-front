4713226f2ca6085dd96980cf14120293d19d8c37
Jannik <jannik@picoscratch.de>
2025-09-23 16:21:46 +0200

    Revert "🚧 Selector for course type"
    This reverts commit d6de184a20984b775fd430e948c5b7b4bacb20ee.

--- src/routes/school/[schoolcode]/courses/+page.svelte
+++ src/routes/school/[schoolcode]/courses/+page.svelte
@@ -6,73 +6,18 @@
 	import { myProfile, schooldata } from "$stores/stores";
 	import { ws } from "$stores/wsStore";
 	import PromptDialog from "$components/dialogs/PromptDialog.svelte";
-  import Dialog from "$components/dialogs/Dialog.svelte";
 	import ConfirmDialog from "$components/dialogs/ConfirmDialog.svelte";
-  import CodeIcon from "svelte-fluentui-icons/icons/Code_Filled.svelte";
-  import BeakerIcon from "svelte-fluentui-icons/icons/Beaker_Filled.svelte";
-  import Selector from "$components/Selector.svelte";
-  import SelectorButton from "$components/SelectorButton.svelte";
 
 	let prompt: PromptDialog;
-  let newCourseDialogShown = false;
 	let confirm: ConfirmDialog;
-  let newCourseNameInput: HTMLInputElement;
-  let codingSelected = true;
-  let chemistrySelected = false;
-
-  function createNewCourse(name) {
-    ws.send({type: "addCourse", name});
-  }
 </script>
 
 <svelte:head>
 	<title>PicoScratch Manager | Kurse</title>
 </svelte:head>
 
-<Dialog bind:showDialog={newCourseDialogShown}>
-	<div style="display: flex; align-items: center; flex-direction: column; gap: 10px;">
-		<h2 style="margin: 0; font-size: 2rem; text-align: center;">Neuen Kurs erstellen</h2>
-		<input type="text" placeholder="Kursname" bind:this={newCourseNameInput} required on:keyup={(e) => {
-			if(e.key === "Enter") {
-        // Submit
-        createNewCourse(newCourseNameInput.value);
-        newCourseNameInput.value = "";
-        newCourseDialogShown = false;
-      }
-		}}>
-    <Selector>
-      <SelectorButton bind:selected={codingSelected} on:click={() => {
-        codingSelected = true;
-        chemistrySelected = false;
-      }}>
-        <CodeIcon size="40" />
-        Programmieren
-      </SelectorButton>
-      <SelectorButton bind:selected={chemistrySelected} on:click={() => {
-        codingSelected = false;
-        chemistrySelected = true;
-      }}>
-        <BeakerIcon size="40" />
-        Chemie
-      </SelectorButton>
-    </Selector>
-		<div style="display: flex; gap: 10px;">
-			<button on:click={() => {
-        createNewCourse(newCourseNameInput.value);
-        newCourseNameInput.value = "";
-        newCourseDialogShown = false;
-      }}>OK</button>
-			<button on:click={() => {
-        newCourseNameInput.value = "";
-        newCourseDialogShown = false;
-      }}>Abbrechen</button>
-		</div>
-	</div>
-</Dialog>
 <PromptDialog bind:this={prompt} />
 <ConfirmDialog bind:this={confirm} />
-
-
 
 <div>
 	{#each $schooldata.courses as course}
@@ -107,12 +52,9 @@
 		<Card>
 			<div style="display: flex; align-items: center; justify-content: center; height: 100%;">
 				<button on:click={async () => {
-					// const name = await prompt.prompt("Neuen Kurs erstellen", { placeholder: "Kursname" });
-					// if(!name) return;
-					// ws.send({ type: "addCourse", name });
-          codingSelected = true;
-          chemistrySelected = false;
-          newCourseDialogShown = true;
+					const name = await prompt.prompt("Neuen Kurs erstellen", { placeholder: "Kursname" });
+					if(!name) return;
+					ws.send({ type: "addCourse", name });
 				}}>
 					<Add_Filled size="40" />
 				</button>
